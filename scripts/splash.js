b4674496c8ce888e5704dc9e8f13e6e7d77b474b
Caiquekola <caiquekola@hptmail.com>
2024-08-04 13:20:09 -0300

    Etapa 0

--- scripts/splash.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        window.location.href = 'index.html'; // Redireciona para index.html
-    }, 5000); // 5 segundos de tela splash
-});
